ff6da45c28dd57e39bf5380a07de7ec5445bfb4e
Denzelzu <zubovichden@gmail.com>
2025-10-17 01:20:57 +0300

    2-commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,90 @@
 package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func OutputHTMLHandler(res http.ResponseWriter, req *http.Request) {
+
+	http.ServeFile(res, req, "index.html")
+
+	file, err := os.Open("../index.html") // открываем файл index.html
+	if err != nil {                       // в случае возникновения ошибки - выводим сообщение и статус ошибки
+		http.Error(res, "не удалось найти index.html:"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	io.Copy(res, file) // копиурем содержимое файла указанного выше, и передаем в HTTP-ответ
+}
+func UpLoadHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "метод не разрешен", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Парсинг HTML-формы (лимит 10MB)
+	err := req.ParseMultipartForm(10 << 20)
+	if err != nil {
+		log.Println("Ошибка при парсинге формы:", err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Файлы в форме:", req.MultipartForm.File)
+
+	file, header, err := req.FormFile("myFile")
+	if err != nil {
+		log.Println("Ошибка при получении файла:", err)
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Чтение данных из файла
+	dataFile, err := io.ReadAll(file)
+	if err != nil {
+		log.Println("Ошибка при чтении файла:", err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fileContent := string(dataFile)
+
+	convertedText, err := service.ConvertData(fileContent) // конвертация данных через пакет service
+	if err != nil {
+		log.Println("Ошибка конвертации:", err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// создание локального файла
+	ext := filepath.Ext(header.Filename)
+	newFilename := "converted_" + time.Now().UTC().Format("20060102_150405") + ext
+
+	outFile, err := os.Create(newFilename)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	//  запись результата конвертации в локальный файл
+	_, err = outFile.WriteString(convertedText)
+	if err != nil {
+		log.Println("Ошибка при записи результата конвертации:", err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//  вернуть результат конвертации строки пользователю
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// Здесь статус 200 OK будет установлен неявно при первом res.Write()
+	res.Write([]byte("Конвертация завершена. Результат сохранен в файл: " + newFilename + "\n\n"))
+	res.Write([]byte(convertedText))
+
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func MorseCode(data string) bool {
+
+	// азбука Морзе может содержать такие символы как ".", "-", "/", " "
+	// проводится проверка каждого входящего символа на совпадение символов из азбуки,
+	// если выявится другой символ, то это не код Морзе
+
+	for _, char := range data {
+		if char != '.' && char != '-' && char != '/' && char != ' ' {
+			// данный символ не является частью азбуки Морзе
+			return false
+		}
+	}
+	return strings.ContainsAny(data, ".-") // проверяем, чтобы данные не содержали только пробелы или слэши, в случае с проверкой выше
+}
+
+func ConvertData(data string) (string, error) {
+
+	data = strings.TrimSpace(data) // Удаляем лишние пробелы
+	if data == "" {
+		return "", errors.New("входная строка пуста или состоит только из пробельных символов")
+	}
+	if MorseCode(data) { // проверяем соотвветствует ли содержимое Морзе
+		return morse.ToText(data), nil
+	}
+
+	return morse.ToMorse(data), nil
+}
