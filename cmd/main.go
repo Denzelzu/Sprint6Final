e9f1745fca23d74c89bd6cf776711ee7816caa5c
Denzelzu <zubovichden@gmail.com>
2025-10-17 00:31:04 +0300

    first-commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "[MORSE_SERVER]", log.LstdFlags)
+
+	srv := server.MyServer(logger)
+
+	logger.Println("Сервер запускается на http://localhost:8080")
+	err := srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
 }
